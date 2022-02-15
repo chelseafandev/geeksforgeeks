e22e35a33dc9b72aa4ddda5638226b689382de33
chelseafandev <chelseafandev@gmail.com>
2022-02-15 23:41:01 +0900

    docs: geeksforgeeks 배열을 활용한 다익스트라 알고리즘

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,13 +1,8 @@
-// A C++ program for Dijkstra's single source shortest path algorithm.
-// The program is for adjacency matrix representation of the graph
 #include <iostream>
 #include <limits.h>
 
-// Number of vertices in the graph
 #define NUMBER_OF_VERTICES 9
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
 int minDistance(int dist[], bool sptSet[])
 {
 
@@ -21,61 +16,49 @@ int minDistance(int dist[], bool sptSet[])
     return min_index;
 }
 
-// A utility function to print the constructed distance array
-void printSolution(int dist[])
-{
-    std::cout << "Vertex \t Distance from Source" << std::endl;
-    for (int i = 0; i < NUMBER_OF_VERTICES; i++)
-        std::cout << i << " \t\t" << dist[i] << std::endl;
-}
-
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
 void dijkstra(int graph[NUMBER_OF_VERTICES][NUMBER_OF_VERTICES], int src)
 {
-    // The output array. dist[i] will hold the shortest distance from src to i
+    // 출발지에서 i까지의 최단 거리를 저장하는 배열
     int dist[NUMBER_OF_VERTICES];
 
-    // sptSet[i] will be true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
+    // 버텍스 i가 최소 신장 트리에 포함되어있거나 출발지에서 버텍스 i까지의 최소 거리를 구한 경우에는 sptSet[i]값이 true
     bool sptSet[NUMBER_OF_VERTICES];
 
-    // Initialize all distances as INFINITE and stpSet[] as false
+    // dist 및 sptSet 배열 초기화(dist는 매우 큰값으로, stpSet은 false)
     for (int i = 0; i < NUMBER_OF_VERTICES; i++)
     {
         dist[i] = INT_MAX;
         sptSet[i] = false;
     }
 
-    // Distance of source vertex from itself is always 0
+    // dist[src](자기 자신) 값은 항상 0
     dist[src] = 0;
 
-    // Find shortest path for all vertices
+    // 모든 버텍스들에 대해 최단 거리를 구한다
     for (int count = 0; count < NUMBER_OF_VERTICES - 1; count++)
     {
-        // Pick the minimum distance vertex from the set of vertices not
-        // yet processed. u is always equal to src in the first iteration.
+        // 1. 아직 처리되지 않은 버텍스들의 집합에서 가장 최소 거리를 갖는 버텍스를 선택
         int u = minDistance(dist, sptSet);
 
-        // Mark the picked vertex as processed
+        // 2. 처리가 된 버텍스는 마킹
         sptSet[u] = true;
 
-        // Update dist value of the adjacent vertices of the picked vertex.
+        // 3. 선택된 버텍스의 인접한 버텍스들의 dist 값 업데이트
         for (int v = 0; v < NUMBER_OF_VERTICES; v++)
-
-            // Update dist[v] only if is not in sptSet, there is an edge from
-            // u to v, and total weight of path from src to v through u is
-            // smaller than current value of dist[v]
+        {
+            // dist[v] 값은 버텍스 v가 sptSet에 포함되어있지 않고 버텍스 u에서 v까지의 edge가 존재하며,
+            // 출발지에서 u를 거쳐 v로 가는 경로의 총 가중치가 dist[v]의 현재 값보다 더 작은 경우에만 업데이트
             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+            {
                 dist[v] = dist[u] + graph[u][v];
+            }
+        }
     }
-
-    // print the constructed distance array
-    printSolution(dist);
 }
 
 int main()
 {
-    // adjacency matrix representation of the graph (row: source, column: destination)
+    // 인접행렬로 그래프를 표현(행:출발지 열:도착지)
     int graph[NUMBER_OF_VERTICES][NUMBER_OF_VERTICES] = {
             {0, 4, 0, 0, 0, 0, 0, 8, 0},
             {4, 0, 8, 0, 0, 0, 0, 11, 0},
